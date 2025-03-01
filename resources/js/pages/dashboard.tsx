81b8c36989e1ac391fbd320d892016f3924c0a09
zobirofkir <zobirofkir19@gmail.com>
2025-03-01 17:55:19 +0000

    remove place holder

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,5 +1,4 @@
 import StateOverview from '@/components/state-overview';
-import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
